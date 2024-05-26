45b6175bae102047916165ebf3df6c38b09db167
Daniel Rodrigues <adanielluiz71@gmail.com>
2024-05-26 14:16:25 -0300

    alterando feing client

--- BACKEND/src/main/java/com/drodrigues17/coracaodascartas/client/CartasClient.java
+++ BACKEND/src/main/java/com/drodrigues17/coracaodascartas/client/CartasClient.java
@@ -1,7 +1,12 @@
 package com.drodrigues17.coracaodascartas.client;
 
 import com.drodrigues17.coracaodascartas.dto.api.YGOCardApiData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@FeignClient(name = "ygprodeck", url = "https://db.ygoprodeck.com/api/v7/cardinfo.php")
 public interface CartasClient {
+
+    @GetMapping
     YGOCardApiData buscarTodasCartas();
 }
--- BACKEND/src/main/java/com/drodrigues17/coracaodascartas/client/DecksClient.java
+++ BACKEND/src/main/java/com/drodrigues17/coracaodascartas/client/DecksClient.java
@@ -1,11 +1,14 @@
 package com.drodrigues17.coracaodascartas.client;
 
 import com.drodrigues17.coracaodascartas.dto.api.DeckApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-
+@FeignClient(name = "urldecks", url = "https://db.ygoprodeck.com/api/v7/cardsets.php")
 public interface DecksClient {
 
+    @GetMapping
     List<DeckApiResponse> buscarTodosDecks();
 }
